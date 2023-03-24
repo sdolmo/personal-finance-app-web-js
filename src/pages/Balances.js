b1ef3cb7654353bfc8ae544368effc1cb140ab98
Sylvia Dolmo <sylviadolmo@gmail.com>
2023-03-24 18:01:32 -0400

    feat(react-router): implement react-router (#17)
    Add pages for routes and organize components into their respective pages

---
+++ src/pages/Balances.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Balances() {
+  return (
+    <section>
+      <h2>Balances Page</h2>
+    </section>
+  );
+}
---
+++ src/pages/Home.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <section>
+      <h2>Home Page</h2>
+    </section>
+  );
+}
