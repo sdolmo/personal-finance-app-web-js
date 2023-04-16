09022e5a55a07dcf0ba81956c4df623af63d2dc1
sdolmo <sylviadolmo@gmail.com>
2023-04-16 15:37:53 -0400

    fix(yearmonthselector): change month selector based on year selected
    Depending on the year selected there is a different range on months to chose from. This change
    updates the month range in the selector from the provided mock data.

--- src/components/YearMonthSelector.jsx
+++ src/components/YearMonthSelector.jsx
@@ -1,19 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import MOCK_DATA from "../data/MOCK_DATA2.json";
 
 export default function YearMonthSelector() {
+  const [yearChosen, setYearChosen] = useState(0);
+  const [monthRange, setMonthRange] = useState([]);
+  const [monthChosen, setMonthChosen] = useState(0);
   const data = MOCK_DATA;
 
-  const years = data.map((transaction) => transaction.date.slice(-4));
+  const years = data.map((transaction) => transaction.date.slice(-4)).sort();
   const uniqueYears = years.filter(
     (value, index, array) => array.indexOf(value) === index
   );
 
+  function handleYearChange(year) {
+    setYearChosen(year);
+
+    const filteredDataByYear = data.filter((transaction) => {
+      const dataYear = transaction.date.slice(-4);
+      return dataYear === year;
+    });
+    const allMonthRange = filteredDataByYear.map((transaction) =>
+      transaction.date.slice(0, 2)
+    );
+
+    const newMonthRange = Array.from(new Set(allMonthRange)).sort();
+    setMonthRange([...newMonthRange]);
+
+    if (!newMonthRange.includes(monthChosen)) {
+      setMonthChosen(0);
+    }
+  }
+
   return (
     <section id="date-selector">
       <label htmlFor="year">Year</label>
-      <select name="year" id="year">
-        <option value="">--Please select an year</option>
+      <select
+        name="year"
+        id="year"
+        value={yearChosen}
+        onChange={(e) => handleYearChange(e.target.value)}
+      >
+        <option value="">--Please select a year</option>
         {uniqueYears.map((year) => {
           return (
             <option key={year} value={year}>
@@ -23,11 +50,20 @@ export default function YearMonthSelector() {
         })}
       </select>
       <label htmlFor="month">Month</label>
-      <select name="month" id="month">
-        <option value="">--Please select an month</option>
-        <option value="1">1</option>
-        <option value="2">2</option>
-        <option value="3">3</option>
+      <select
+        name="month"
+        id="month"
+        value={monthChosen}
+        onChange={(e) => setMonthChosen(e.target.value)}
+      >
+        <option value="">--Please select a month</option>
+        {monthRange.map((month) => {
+          return (
+            <option key={month} value={month}>
+              {month}
+            </option>
+          );
+        })}
       </select>
     </section>
   );
