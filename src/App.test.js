6128dcfb4b9e017b216310b7dc230e29e18ff417
sdolmo <sylviadolmo@gmail.com>
2023-04-16 12:38:42 -0400

    test(app): add tests for App navigation
    Inicialize tests for personal finance app
    
    fix #30

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
+import React from "react";
 import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
 import App from "./App";
 
-test("renders learn react link", () => {
+/* global test, expect */
+/* eslint no-undef: "error" */
+test("renders personal finance link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/ personal finance/i);
   expect(linkElement).toBeInTheDocument();
 });
