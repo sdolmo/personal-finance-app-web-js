f2fdf7b22f6f13c5e3f0c3c69463b99d2f591350
sdolmo <sylviadolmo@gmail.com>
2023-08-01 15:43:11 -0400

    feat(trasactions): use AG Grid for transactions table
    Use AG Grid instead of headless component for trnasactions table

---
+++ src/components/TransactionsTable.jsx
@@ -0,0 +1,44 @@
+import React, { useState, useMemo, useRef } from "react";
+import { AgGridReact } from "ag-grid-react";
+import MOCK_DATA from "../data/MOCK_DATA_726.json";
+
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-alpine.css";
+
+export const TransactionsTable = () => {
+  const gridRef = useRef();
+  const [rowData] = useState(MOCK_DATA);
+  const [columnDefs] = useState([
+    { field: "date" },
+    { field: "amount" },
+    { field: "merchant_name" },
+    { field: "category" },
+  ]);
+
+  const defaultColDef = useMemo(
+    () => ({
+      flex: 1,
+      filter: true,
+      filterParams: {
+        debounce: 0,
+        buttons: ["reset"],
+      },
+    }),
+    []
+  );
+
+  return (
+    <div
+      id="transactions"
+      className="ag-theme-alpine"
+      style={{ height: 400, width: 800 }}
+    >
+      <AgGridReact
+        ref={gridRef}
+        rowData={rowData}
+        columnDefs={columnDefs}
+        defaultColDef={defaultColDef}
+      ></AgGridReact>
+    </div>
+  );
+};
