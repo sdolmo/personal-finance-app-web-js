e2df4ff2886d64c0fcd14f28bdc28fa91b146656
sdolmo <sylviadolmo@gmail.com>
2023-08-01 18:54:09 -0400

    feat(distribution): update Distribution Chart
    Update distribution chart with ag grid

--- src/components/Distribution.jsx
+++ src/components/Distribution.jsx
@@ -1,29 +1,60 @@
-import React from "react";
-import { Chart } from "react-google-charts";
+import React, { useState } from "react";
+import { AgChartsReact } from "ag-charts-react";
+import Data from "../data/MOCK_DATA_726.json";
 
-export const data = [
-  ["Task", "Hours per Day"],
-  ["Work", 11],
-  ["Eat", 2],
-  ["Commute", 2],
-  ["Watch TV", 2],
-  ["Sleep", 7],
-];
+class Catgory {
+  constructor(category) {
+    this.category = category;
+    this.budget = this.calculateBudget();
+    this.percent = this.calculatePercent(this.budget);
+  }
 
-export const options = {
-  title: "My Daily Activities",
-};
+  calculateBudget() {
+    const categoryList = Data.filter(
+      (transaction) => transaction.category === this.category
+    );
+
+    const categorySpend = Number(
+      categoryList
+        .reduce((accum, transaction) => accum + transaction.amount, 0)
+        .toFixed(2)
+    );
+
+    return categorySpend;
+  }
+
+  calculatePercent(spend) {
+    const calc = (spend / totalSpend) * 100;
+    return Number(calc.toFixed(2));
+  }
+}
+
+const totalSpend = Number(
+  Data.reduce((accum, transaction) => accum + transaction.amount, 0).toFixed(2)
+);
+const allCategories = Data.map((t) => t.category);
+const categories = Array.from(new Set(allCategories));
+const optionData = categories.map((c) => new Catgory(c));
 
 export default function Distribution() {
+  const [options, setOptions] = useState({
+    data: optionData,
+    series: [
+      {
+        type: "pie",
+        calloutLabelKey: "category",
+        angleKey: "percent",
+        innerRadiusOffset: -70,
+      },
+    ],
+  });
+
+  console.log(categories);
+  console.log(optionData);
+
   return (
     <section id="distribution">
-      <Chart
-        chartType="PieChart"
-        data={data}
-        options={options}
-        width={"100%"}
-        height={"400px"}
-      />
+      <AgChartsReact options={options} />
     </section>
   );
 }
