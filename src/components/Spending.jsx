e1631a84b38b612a58e78683bebe70af6a6888f4
Sylvia Dolmo <sylviadolmo@gmail.com>
2023-08-01 20:29:52 -0400

    feat(transactions): use AG Grid for transactions table (#36)
    * feat(trasactions): use AG Grid for transactions table
    
    Use AG Grid instead of headless component for trnasactions table
    
    * feat(distribution): update Distribution Chart
    
    Update distribution chart with ag grid
    
    * feat(update spend chart): update spend chart with mock data
    
    use ag charts for spend bar chart

--- src/components/Spending.jsx
+++ src/components/Spending.jsx
@@ -1,18 +1,67 @@
-import React from "react";
-import { Chart } from "react-google-charts";
+import React, { useState } from "react";
+import { AgChartsReact } from "ag-charts-react";
+import Data from "../data/MOCK_DATA_726.json";
 
-export const data = [
-  ["Element", "Density", { role: "style" }],
-  ["Copper", 8.94, "#b87333"], // RGB value
-  ["Silver", 10.49, "silver"], // English color name
-  ["Gold", 19.3, "gold"],
-  ["Platinum", 21.45, "color: #e5e4e2"], // CSS-style declaration
-];
+// Get the latest year of the data
+//  string value (0-3)
+const dates = Data.map((t) => t.date);
+// console.log(dates);
+const years = dates.map((date) => date.substring(0, 4));
+// console.log(Array.from(new Set(years)).sort());
+const latestYear = Array.from(new Set(years)).sort().at(-1);
+// console.log(latestYear);
+const latestYearData = Data.filter(
+  (t) => t.date.substring(0, 4) === latestYear
+);
+console.log(latestYearData);
+
+// const years = dates.
+// Get the latest 4 months of that year
+//  string value (5-6)
+const latestYearMonths = latestYearData.map((t) => t.date.substring(5, 7));
+const sortedMonths = Array.from(new Set(latestYearMonths)).sort();
+const latestSortedMonths = sortedMonths.slice(sortedMonths.length - 4);
+// console.log(latestSortedMonths);
+// Get all amounts and add for same month
+const currentYearMonth = latestSortedMonths.map(
+  (mth) => `${latestYear}-${mth}`
+);
+console.log(currentYearMonth);
+const result = [];
+for (const prefix of currentYearMonth) {
+  const data = latestYearData.filter((t) => t.date.includes(prefix));
+  console.log(data);
+  const totalMthAmt = data.reduce((acc, t) => acc + t.amount, 0);
+  console.log(totalMthAmt);
+  console.log(result.push({ prefix, amount: totalMthAmt }));
+  console.log(result);
+}
+console.log(result);
+
+// xKey = months
+// yKey = Spend in month
 
 export default function Spending() {
+  const [options] = useState({
+    title: {
+      text: "Spending",
+    },
+    subtitle: {
+      text: "in the last four months",
+    },
+    data: result,
+    series: [
+      {
+        type: "column",
+        xKey: "prefix",
+        yKey: "amount",
+      },
+    ],
+  });
+
   return (
     <section id="spending">
-      <Chart chartType="ColumnChart" width="100%" height="400px" data={data} />
+      <AgChartsReact options={options} />
     </section>
   );
 }
--- src/pages/Transactions.js
+++ src/pages/Transactions.js
@@ -1,17 +1,15 @@
 import React from "react";
-import YearMonthSelector from "../components/YearMonthSelector";
 import Distribution from "../components/Distribution";
 import Spending from "../components/Spending";
-import { BasicTable } from "../components/BasicTable";
+import { TransactionsTable } from "../components/TransactionsTable";
 
 export default function Transactions() {
   return (
     <>
       <h2>Transactions Page</h2>
-      <YearMonthSelector />
       <Distribution />
       <Spending />
-      <BasicTable />
+      <TransactionsTable />
     </>
   );
 }
