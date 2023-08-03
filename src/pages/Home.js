be011e05f4c53d6f49b6c6f66d05a03d7eff5143
Sylvia Dolmo <sylviadolmo@gmail.com>
2023-08-03 17:06:32 -0400

    feat(css): update styling (#43)
    Add styling to align transactions, Update Home page styling, Update README

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,8 +2,26 @@ import React from "react";
 
 export default function Home() {
   return (
-    <section>
-      <h2>Home Page</h2>
-    </section>
+    <>
+      <h2></h2>
+      <section id="banner"></section>
+      <div className="container">
+        <section className="box">
+          <h3>Secure</h3>
+          <p>Safely connect banking data by loging into your account</p>
+        </section>
+        <section className="box">
+          <h3>Transactions</h3>
+          <p>View latest transactions data from your connected accounts</p>
+        </section>
+        <section className="box">
+          <h3>Budgeting</h3>
+          <p>
+            Compare spending over the last few months and review distribution of
+            spending habits
+          </p>
+        </section>
+      </div>
+    </>
   );
 }
