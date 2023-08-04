50b8600cd3c764fcb0b33312a87447c1bcc7116e
sdolmo <sylviadolmo@gmail.com>
2023-08-03 23:51:59 -0400

    fix(fix): middleware

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,11 +1,14 @@
-import express, { Router } from "express";
+import express from "express";
+import * as dotenv from "dotenv";
 import serverless from "serverless-http";
+import bodyParser from "body-parser";
+dotenv.config();
 
 const api = express();
 
-const router = Router();
-router.get("/hello", (req, res) => res.json({ data: "Hello World!" }));
+api.use(bodyParser.urlencoded({ extended: false }));
+api.use(bodyParser.json());
 
-api.use("/api/", router);
+api.get("/hello", (req, res) => res.json({ data: "Hello World!" }));
 
 export const handler = serverless(api);
