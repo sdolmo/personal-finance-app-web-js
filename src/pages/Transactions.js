176440691a3358645037064f064c77157ddf2433
Sylvia Dolmo <sylviadolmo@sylvias-mbp.lan>
2023-08-08 00:01:33 -0400

    style: 💄 transactions
    style transactions page

--- src/pages/Transactions.js
+++ src/pages/Transactions.js
@@ -3,15 +3,18 @@ import Distribution from "../components/Distribution";
 import Spending from "../components/Spending";
 import TransactionsTable from "../components/TransactionsTable";
 import { TransactionsDataContext } from "../Context";
+import "../assets/styles/Transactions.css";
 
 export default function Transactions() {
   const data = useContext(TransactionsDataContext);
 
   return (
     <>
-      <Distribution transactions={data} />
-      <Spending transactions={data} />
-      <TransactionsTable transactions={data} />
+      <section className="transactions">
+        <Distribution transactions={data} />
+        <Spending transactions={data} />
+        <TransactionsTable transactions={data} />
+      </section>
     </>
   );
 }
